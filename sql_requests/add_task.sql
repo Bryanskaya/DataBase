62ea1ffefdf7a839a22e2fe87affbc33ab07bbde
Bryanskaya <bryanskayakatya@yandex.ru>
2020-09-29 14:04:57 +0300

    ADD: additional tasks

---
+++ sql_requests/add_task.sql
@@ -0,0 +1,47 @@
+DROP TABLE Table1 CASCADE;
+DROP TABLE Table2 CASCADE;
+
+CREATE TEMP TABLE IF NOT EXISTS Table1 
+(
+	id INT,
+	var1 TEXT,
+	valid_from_dttm date,
+	valid_to_dttm date
+);
+
+CREATE TEMP TABLE IF NOT EXISTS Table2
+(
+	id INT,
+	var2 TEXT,
+	valid_from_dttm date,
+	valid_to_dttm date
+);
+
+INSERT INTO Table1(id, var1, valid_from_dttm, valid_to_dttm) VALUES (1, 'A', '2018-09-01', '2018-09-15');
+INSERT INTO Table1(id, var1, valid_from_dttm, valid_to_dttm) VALUES (1, 'B', '2018-09-16', '2018-10-11');
+INSERT INTO Table1(id, var1, valid_from_dttm, valid_to_dttm) VALUES (1, 'C', '2018-10-12', '2018-10-14');
+INSERT INTO Table1(id, var1, valid_from_dttm, valid_to_dttm) VALUES (1, 'D', '2018-10-15', '5999-12-31');
+
+INSERT INTO Table2(id, var2, valid_from_dttm, valid_to_dttm) VALUES (1, 'A', '2018-09-01', '2018-09-18');
+INSERT INTO Table2(id, var2, valid_from_dttm, valid_to_dttm) VALUES (1, 'B', '2018-09-19', '2018-09-21');
+INSERT INTO Table2(id, var2, valid_from_dttm, valid_to_dttm) VALUES (1, 'C', '2018-09-22', '2018-09-30');
+INSERT INTO Table2(id, var2, valid_from_dttm, valid_to_dttm) VALUES (1, 'D', '2018-10-01', '5999-12-31');
+
+SELECT tbl1.id,
+	   var1, var2,
+	   CASE 
+	   		WHEN tbl1.valid_from_dttm > tbl2.valid_from_dttm::timestamp THEN tbl1.valid_from_dttm
+			ELSE tbl2.valid_from_dttm
+	   END,
+	   CASE 
+	   		WHEN tbl1.valid_to_dttm < tbl2.valid_to_dttm THEN tbl1.valid_to_dttm
+			ELSE tbl2.valid_to_dttm
+	   END
+--SELECT *
+FROM Table1 tbl1 JOIN Table2 tbl2 ON 
+	tbl1.id = tbl2.id AND
+	(tbl2.valid_from_dttm BETWEEN tbl1.valid_from_dttm AND tbl1.valid_to_dttm OR
+     tbl1.valid_from_dttm BETWEEN tbl2.valid_from_dttm AND tbl2.valid_to_dttm);
+
+--SELECT * FROM Table1;
+--SELECT * FROM Table2;
