db2f0e8e11f09b4b4f664ff48bdb744c02490001
Bryanskaya <bryanskayakatya@yandex.ru>
2020-11-10 21:09:53 +0300

    CORRECTED: procedures

--- sql_requests/procedures.sql
+++ sql_requests/procedures.sql
@@ -2,6 +2,7 @@
 -- Хранимые процедуры
 
 -- 1) Хранимая процедура без параметров или с параметрами
+-- Добавить хохяйство в таблицу хозяйств (название, площадь, максимальное количество секторов подаются)
 CREATE OR REPLACE PROCEDURE InsertHuntingGround(gr_name VARCHAR(30), s NUMERIC, max_num_s INTEGER)
 AS $$
 DECLARE 
@@ -11,19 +12,21 @@ BEGIN
 	FROM hunting_grounds
 	INTO temp_id;
 	
-	--INSERT INTO hunting_grounds VALUES (temp_id, gr_name, s, max_num_s);
+	INSERT INTO hunting_grounds VALUES (temp_id, gr_name, s, max_num_s);
 	RAISE INFO 'Запись добавлена';
 END;
 $$
 LANGUAGE PLpgSql;
 
-CALL InsertHuntingGround('Test_ground', 5000, 10)
+CALL InsertHuntingGround('Test_ground', 5000, 10);
+
 SELECT *
 FROM hunting_grounds
 
 -- 2) Рекурсивную хранимую процедуру или хранимую процедур с рекурсивным ОТВ
 -- Вывести все секторы, принадлежащие тому же хозяйству, что и сектор под номером, которое подали на вход
 DROP TABLE temp_table CASCADE;
+
 CREATE TABLE IF NOT EXISTS temp_table(
 	id SERIAL PRIMARY KEY,
 	square NUMERIC CONSTRAINT valid_square CHECK (square > 0),
@@ -57,4 +60,68 @@ SELECT *
 FROM temp_table;
 
 -- 3) Хранимая процедура с курсором
+-- Изменить зарплату у егерей со стажем работы большем или равным ... (коэффициент и стаж подаётся в процедуру)
+DROP TABLE temp_huntsmen;
+
+SELECT *
+INTO TEMP temp_huntsmen
+FROM huntsmen
+ORDER BY id;
+
+CREATE OR REPLACE PROCEDURE ChangeHuntsmenSalary(num NUMERIC, expr INTEGER)
+AS $$
+DECLARE myCursor CURSOR
+	FOR
+		SELECT *
+		FROM temp_huntsmen
+		WHERE experience >= expr;
+		param RECORD;
+BEGIN
+	OPEN myCursor;
+	LOOP
+		FETCH myCursor INTO param;
+		EXIT WHEN NOT FOUND;
+		
+		UPDATE temp_huntsmen
+		SET salary = salary * num
+		WHERE temp_huntsmen.id = param.id;
+	END LOOP;
+	CLOSE myCursor;
+END
+$$
+LANGUAGE PLpgSql;
+
+CALL ChangeHuntsmenSalary(1.05, 30);
+
+SELECT *
+FROM temp_huntsmen
+ORDER BY id;
+
 -- 4) Хранимая процедура доступа к метаданным
+-- Вывести название поля и его размер, название таблицы подаётся в процедуру
+CREATE OR REPLACE PROCEDURE ShowSize(name_table varchar)
+AS $$
+DECLARE myCursor CURSOR
+	FOR
+		SELECT column_name, size
+		from
+		(
+			select column_name, pg_column_size(cast(column_name as varchar)) as size 
+			from information_schema.columns
+			where table_name = name_table
+		) AS size_info;
+		param RECORD;
+BEGIN
+	OPEN myCursor;
+	LOOP
+		FETCH myCursor INTO param;
+		EXIT WHEN NOT FOUND;
+		
+		RAISE INFO 'column: %   size: %', param.column_name, param.size;
+	END LOOP;
+	CLOSE myCursor;
+END
+$$
+LANGUAGE PLpgSql;
+
+CALL ShowSize('hunters');
